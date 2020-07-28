aa591609bd708b8f12d76b80606288306a2017e1
Benedict Busole <migarajun@outlook.com>
2020-07-28 11:02:21 +0200

    project one

---
+++ Medusa.X/mcc_generated_files/pin_manager.h
@@ -0,0 +1,237 @@
+/**
+  PIN MANAGER Generated Driver File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the generated manager file for the PIC24 / dsPIC33 / PIC32MM MCUs device.  This manager
+    configures the pins direction, initial state, analog setting.
+
+  @Description:
+    This source file provides implementations for PIN MANAGER.
+    Generation Information :
+        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.167.0
+        Device            :  PIC24FJ128GA006
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 v1.50
+        MPLAB 	          :  MPLAB X v5.35
+*/
+
+/*
+    (c) 2020 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+#ifndef _PIN_MANAGER_H
+#define _PIN_MANAGER_H
+/**
+    Section: Includes
+*/
+#include <xc.h>
+
+/**
+    Section: Device Pin Macros
+*/
+/**
+  @Summary
+    Sets the GPIO pin, RF5, high using LATF5.
+
+  @Description
+    Sets the GPIO pin, RF5, high using LATF5.
+
+  @Preconditions
+    The RF5 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RF5 high (1)
+    MEDUSA_SetHigh();
+    </code>
+
+*/
+#define MEDUSA_SetHigh()          (_LATF5 = 1)
+/**
+  @Summary
+    Sets the GPIO pin, RF5, low using LATF5.
+
+  @Description
+    Sets the GPIO pin, RF5, low using LATF5.
+
+  @Preconditions
+    The RF5 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Set RF5 low (0)
+    MEDUSA_SetLow();
+    </code>
+
+*/
+#define MEDUSA_SetLow()           (_LATF5 = 0)
+/**
+  @Summary
+    Toggles the GPIO pin, RF5, using LATF5.
+
+  @Description
+    Toggles the GPIO pin, RF5, using LATF5.
+
+  @Preconditions
+    The RF5 must be set to an output.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Toggle RF5
+    MEDUSA_Toggle();
+    </code>
+
+*/
+#define MEDUSA_Toggle()           (_LATF5 ^= 1)
+/**
+  @Summary
+    Reads the value of the GPIO pin, RF5.
+
+  @Description
+    Reads the value of the GPIO pin, RF5.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    uint16_t portValue;
+
+    // Read RF5
+    postValue = MEDUSA_GetValue();
+    </code>
+
+*/
+#define MEDUSA_GetValue()         _RF5
+/**
+  @Summary
+    Configures the GPIO pin, RF5, as an input.
+
+  @Description
+    Configures the GPIO pin, RF5, as an input.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RF5 as an input
+    MEDUSA_SetDigitalInput();
+    </code>
+
+*/
+#define MEDUSA_SetDigitalInput()  (_TRISF5 = 1)
+/**
+  @Summary
+    Configures the GPIO pin, RF5, as an output.
+
+  @Description
+    Configures the GPIO pin, RF5, as an output.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    // Sets the RF5 as an output
+    MEDUSA_SetDigitalOutput();
+    </code>
+
+*/
+#define MEDUSA_SetDigitalOutput() (_TRISF5 = 0)
+
+/**
+    Section: Function Prototypes
+*/
+/**
+  @Summary
+    Configures the pin settings of the PIC24FJ128GA006
+
+  @Description
+    This is the generated manager file for the PIC24 / dsPIC33 / PIC32MM MCUs device.  This manager
+    configures the pins direction, initial state, analog setting.
+
+  @Preconditions
+    None.
+
+  @Returns
+    None.
+
+  @Param
+    None.
+
+  @Example
+    <code>
+    void SYSTEM_Initialize(void)
+    {
+        // Other initializers are called from this function
+        PIN_MANAGER_Initialize();
+    }
+    </code>
+
+*/
+void PIN_MANAGER_Initialize (void);
+
+
+
+#endif
